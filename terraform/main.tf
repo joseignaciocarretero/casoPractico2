9f163d797cc97133eb0ec128c043c97aecd0c06e
joseignaciocarretero <83956668+joseignaciocarretero@users.noreply.github.com>
2021-06-24 18:10:17 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,10 +12,10 @@ terraform {
 # crea un service principal y rellena los siguientes datos para autenticar
 provider "azurerm" {
   features {}
-  subscription_id = "<SUBSCRIPCION ID>"
-  client_id       = "<APP_ID>"
-  client_secret   = "<PASSWORD>"
-  tenant_id       = "<TENANT>"
+  subscription_id = "17b8d1eb-a17d-448b-bbd9-0ee611ad3ec0"
+  client_id       = "25888a55-02b4-4dd8-9086-ec54aa9d6546"
+  client_secret   = "QKU37xsgKZW1fYotTCKiwv_eaRmRoWNhHz"
+  tenant_id       = "899789dc-202f-44b4-8472-a6d40f9eb440"
 }
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group

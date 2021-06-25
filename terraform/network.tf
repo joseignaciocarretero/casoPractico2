f4757460aaaad8e1af98d556a44f8dfb4cd61378
joseignaciocarretero <83956668+joseignaciocarretero@users.noreply.github.com>
2021-06-25 18:39:47 +0200

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -26,7 +26,7 @@ resource "azurerm_subnet" "mySubnet" {
 # Create NIC
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface
 
-resource "azurerm_network_interface" "myNic${var.vms[count.index]}" {
+resource "azurerm_network_interface" "myNic${count.index}" {
   name                = "nic-${var.vms[count.index]}"  
   count               = length(var.vms)
   location            = azurerm_resource_group.rg.location

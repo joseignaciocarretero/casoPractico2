5b101cc21fb28f4b2c6deac0851efbe6e5acf514
joseignaciocarretero <83956668+joseignaciocarretero@users.noreply.github.com>
2021-06-25 18:56:46 +0200

    Update vm.tf

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -1,13 +1,13 @@
 # Creamos una máquina virtual
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine
 
-resource "azurerm_linux_virtual_machine" "myVM1" {
-    name                = "my-first-azure-vm"
+resource "azurerm_linux_virtual_machine" "myVM${var.vms[count.index]}" {
+    name                = "${var.vms[count.index]}"
     resource_group_name = azurerm_resource_group.rg.name
     location            = azurerm_resource_group.rg.location
     size                = var.vm_size
     admin_username      = "adminUsername"
-    network_interface_ids = [ azurerm_network_interface.myNic1.id ]
+    network_interface_ids = [ azurerm_network_interface.myNic[count.index].id ]
     disable_password_authentication = true
 
     #creo la clave pública ssh con el comando "ssh-keygen -t rsa -b 4096"

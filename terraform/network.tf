c48ab9a536daaa32b407f68d286acda50bf5abdb
joseignaciocarretero <83956668+joseignaciocarretero@users.noreply.github.com>
2021-06-25 18:26:36 +0200

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -27,17 +27,18 @@ resource "azurerm_subnet" "mySubnet" {
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface
 
 resource "azurerm_network_interface" "myNic1" {
-  name                = "vmnic1"  
+  name                = "nic-${var.vms[count.index]}"  
+  count               = length(var.vms)
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
     ip_configuration {
-    name                           = "myipconfiguration1"
+    name                           = "ipconf-${var.vms[count.index]}"
     subnet_id                      = azurerm_subnet.mySubnet.id 
     #se podría usar en el allocation "Dynamic" para no poner una ip fija y fuera por dhcp
     private_ip_address_allocation  = "Static"
-    private_ip_address             = "10.0.1.10"
-    public_ip_address_id           = azurerm_public_ip.myPublicIp1.id
+    private_ip_address             = "10.0.1.${count.index +10}"
+    public_ip_address_id           = azurerm_public_ip.myPublicIp[count.index].id
   }
 
     tags = {
@@ -49,8 +50,8 @@ resource "azurerm_network_interface" "myNic1" {
 # IP pública
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/public_ip
 
-resource "azurerm_public_ip" "myPublicIp1" {
-  name                = "vmip1"
+resource "azurerm_public_ip" "myPublicIp${count.index}" {
+  name                = "vmip${count.index}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"

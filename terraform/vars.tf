bf99fc32ae1bf8b30fc70d7b90abf4744801fcf3
joseignaciocarretero <83956668+joseignaciocarretero@users.noreply.github.com>
2021-06-27 20:21:58 +0200

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -12,18 +12,18 @@ variable "vm_size" {
   default = "Standard_A1_v2" # 2 GB ram, 1 CPU, 10 GB Temporary Disk (SSD) 
 }
 
-#tamaño para la máquina virtual Master
-variable "vm_size_Master" {
-  type = string
-  description = "Tamaño de la máquina virtual para Master"
-  default = "Standard_A2_v2" # 4 GB ram, 2 CPU, 20 GB Temporary Disk (SSD) 
-}
-
 #Variable para Máquinas virtuales 
 variable "vms" {
   description = "máquinas virtuales a crear"
   type = list(string)
   default = ["worker01","worker02","nfs"]
+}
+
+#tamaño para la máquina virtual Master
+variable "vm_size_Master" {
+  type = string
+  description = "Tamaño de la máquina virtual para Master"
+  default = "Standard_A2_v2" # 4 GB ram, 2 CPU, 20 GB Temporary Disk (SSD) 
 }
 
 #Variable para la máquina virtual Master 

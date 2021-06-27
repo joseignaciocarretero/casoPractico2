46a688dd81a51a4b5f55d6fcb9857ebc8a6332e1
joseignaciocarretero <83956668+joseignaciocarretero@users.noreply.github.com>
2021-06-27 20:14:17 +0200

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -5,15 +5,30 @@
 #  default = "West Europe"
 #}
 
-#asumimos que todas las máquinas virtuales tienen el mismo tamaño
+#asumimos que todas las máquinas virtuales para Workers y NFS tienen el mismo tamaño
 variable "vm_size" {
   type = string
-  description = "Tamaño de la máquina virtual"
-  default = "Standard_D1_v2" # 3.5 GB, 1 CPU 
+  description = "Tamaño de la máquina virtual para workers y NFS"
+  default = "Standard_A1_v2" # 2 GB ram, 1 CPU, 10 GB Temporary Disk (SSD) 
 }
 
+#tamaño para la máquina virtual Master
+variable "vm_size_Master" {
+  type = string
+  description = "Tamaño de la máquina virtual para Master"
+  default = "Standard_A2_v2" # 4 GB ram, 2 CPU, 20 GB Temporary Disk (SSD) 
+}
+
+#Variable para Máquinas virtuales 
 variable "vms" {
   description = "máquinas virtuales a crear"
   type = list(string)
-  default = ["master","worker01","worker02","nfs"]
+  default = ["worker01","worker02","nfs"]
+}
+
+#Variable para la máquina virtual Master 
+variable "vms_Master" {
+  description = "máquinas virtuales a crear"
+  type = string
+  default = "master"
 }

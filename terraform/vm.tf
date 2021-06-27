cbb82ee4b6d5d989a9ff4a76145f4caaa587f152
joseignaciocarretero <83956668+joseignaciocarretero@users.noreply.github.com>
2021-06-27 20:25:06 +0200

    Update vm.tf

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -45,3 +45,50 @@ resource "azurerm_linux_virtual_machine" "myVM" {
     }
 
 }
+
+# Creamos una máquina virtual para Master
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine
+
+resource "azurerm_linux_virtual_machine" "myVM_master" {
+    name                = "master"
+    resource_group_name = azurerm_resource_group.rg.name
+    location            = azurerm_resource_group.rg.location
+    size                = var.vm_size_Master
+    admin_username      = "adminUsername"
+    network_interface_ids = [ azurerm_network_interface.myNicMaster.id ]
+    disable_password_authentication = true
+
+    #creo la clave pública ssh con el comando "ssh-keygen -t rsa -b 4096"
+    admin_ssh_key {
+        username   = "adminUsername"
+        public_key = file("~/.ssh/id_rsa.pub")
+    }
+
+    os_disk {
+        caching              = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+    plan {
+        name      = "centos-8-stream-free"
+        product   = "centos-8-stream-free"
+        publisher = "cognosys"
+    }
+    
+    #para la práctica uso la misma imagen de centos que se da de ejemplo
+    source_image_reference {
+        publisher = "cognosys"
+        offer     = "centos-8-stream-free"
+        sku       = "centos-8-stream-free"
+        version   = "1.2019.0810"
+    }
+
+    boot_diagnostics {
+        storage_account_uri = azurerm_storage_account.stAccount.primary_blob_endpoint
+    }
+
+    tags = {
+        environment = "CP2"
+    }
+
+}

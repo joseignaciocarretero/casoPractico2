c72708a033e42170604dfe13850f74d735635203
joseignaciocarretero <83956668+joseignaciocarretero@users.noreply.github.com>
2021-06-27 21:07:41 +0200

    Update correccion-vars.tf

--- terraform/correccion-vars.tf
+++ terraform/correccion-vars.tf
@@ -7,7 +7,7 @@ variable "location" {
 variable "storage_account" {
   type = string
   description = "Nombre para la storage account"
-  default = "storageAccount_envcp2"
+  default = "storageaccount_env_cp2"
 }
 
 variable "public_key_path" {

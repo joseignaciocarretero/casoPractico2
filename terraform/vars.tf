7d2aeff1e61095b278caa9b8b1139d1fcbb9951f
joseignaciocarretero <83956668+joseignaciocarretero@users.noreply.github.com>
2021-07-03 08:55:33 +0200

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,22 +1,8 @@
-# movido a correccion-vars
-#variable "location" {
-#  type = string
-#  description = "Región de Azure donde crearemos la infraestructura"
-#  default = "West Europe"
-#}
-
 #asumimos que todas las máquinas virtuales para Worker y NFS tienen el mismo tamaño
 variable "vm_size" {
   type = string
   description = "Tamaño de la máquina virtual para workers y NFS"
   default = "Standard_A1_v2" # 2 GB ram, 1 CPU, 10 GB Temporary Disk (SSD) 
-}
-
-#Variable para Máquinas virtuales 
-variable "vms" {
-  description = "máquinas virtuales a crear"
-  type = list(string)
-  default = ["worker", "nfs"]
 }
 
 #tamaño para la máquina virtual Master
@@ -26,9 +12,16 @@ variable "vm_size_master" {
   default = "Standard_A2_v2" # 4 GB ram, 2 CPU, 20 GB Temporary Disk (SSD) 
 }
 
-#Variable para la máquina virtual Master 
+#Variable para la máquina virtual Master/NFS 
 variable "vms_master" {
   description = "máquinas virtuales a crear"
   type = string
   default = "master"
+}
+
+#Variable para Máquinas virtuales nodos
+variable "vms" {
+  description = "máquinas virtuales a crear"
+  type = list(string)
+  default = ["worker01", "worker02"]
 }

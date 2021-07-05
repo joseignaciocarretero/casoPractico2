848b22d43a9dc320a5326037c461bf4262c059b4
joseignaciocarretero <83956668+joseignaciocarretero@users.noreply.github.com>
2021-07-05 09:51:02 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,8 +58,8 @@ resource "azurerm_managed_disk" "nfs" {
 
 # Asociamos el disco de Persistant Storage a master
 resource "azurerm_virtual_machine_data_disk_attachment" "nfs" {
-  managed_disk_id    = azurerm_managed_disk.nfs.0.id
-  virtual_machine_id = azurerm_virtual_machine.myVM_master.id
+  managed_disk_id    = azurerm_managed_disk.nfs
+  virtual_machine_id = azurerm_virtual_machine.myVM_master
   lun                = "10"
   caching            = "ReadWrite"
 }

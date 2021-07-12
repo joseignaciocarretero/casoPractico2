4396b9abd8f7a26472ad3875e4df47368494ed8d
joseignaciocarretero <83956668+joseignaciocarretero@users.noreply.github.com>
2021-07-12 07:44:43 +0200

    Update deploy.sh

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 # añadir tantas líneas como sean necesarias para el correcto despligue
-ansible-playbook -i hosts -l XXXX playbook
+#ansible-playbook -i hosts -l XXXX playbook
+ansible-playbook -i hosts playbooks/01_deploy_common.yml
+ansible-playbook -i hosts playbooks/02_deploy_nfs.yml
+ansible-playbook -i hosts playbooks/03_deploy_kubernetes.yml
+ansible-playbook -i hosts playbooks/04_deploy_configureKubernetes.yml
+ansible-playbook -i hosts playbooks/06_deploy_ingress.yml
+ansible-playbook -i hosts playbooks/07_deploy_adduser.yml
+ansible-playbook -i hosts playbooks/08_deploy_app.yml
